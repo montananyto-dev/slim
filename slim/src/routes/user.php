89546948424c8e66e7dd1add65ec6fc0cb81cf38
montananyto-dev <montananyto@hotmail.com>
2017-11-30 12:48:31 +0000

    Fully working view/user

--- slim/src/routes/user.php
+++ slim/src/routes/user.php
@@ -16,27 +16,27 @@ $app->get('/view/users',function(){
 
     $query = "SELECT 
 
-users.user_first_name first_name,
-users.user_last_name last_name,
-users.user_password password,
-users.user_email email,
-users.user_phone_number phone_number,
-course.course_name course ,
-users.user_departement departement,
-users.user_about_me about_me
-
-FROM users
-
-inner join users_course
-on users.user_id = users_course.user_id
-inner join course
-on users_course.course_id = course.course_id
-inner join module_course
-on course.course_id = module_course.course_id
-inner join module
-on module_course.module_id = module.module_id
-
-group by users.user_id";
+                users.user_first_name first_name,
+                users.user_last_name last_name,
+                users.user_password password,
+                users.user_email email,
+                users.user_phone_number phone_number,
+                course.course_name course ,
+                users.user_departement departement,
+                users.user_about_me about_me
+                
+                FROM users
+                
+                inner join users_course
+                on users.user_id = users_course.user_id
+                inner join course
+                on users_course.course_id = course.course_id
+                inner join module_course
+                on course.course_id = module_course.course_id
+                inner join module
+                on module_course.module_id = module.module_id
+                
+                group by users.user_id";
 
     $result = $connection->query($query);
 
