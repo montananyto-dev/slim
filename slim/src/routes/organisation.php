33e2e5dd40c9388db3e6130b078d6d74fe7d3196
montananyto-dev <montananyto@hotmail.com>
2017-12-04 23:06:15 +0000

    add/organisation fully working

--- slim/src/routes/organisation.php
+++ slim/src/routes/organisation.php
@@ -16,3 +16,44 @@ $app->get('/view/organisation', function () {
     }
 });
 
+
+$app->post('/add/organisation',function($request, $response)  {
+
+    require_once('dbconnect.php');
+    $connection = connect_db();
+
+    //prepared statements
+    $query = "INSERT INTO kingsub3_FYP.organisation
+  (
+   organisation_name, 
+   organisation_address,
+   organisation_type,
+   organisation_phone_number,
+   organisation_country,
+   organisation_created_at) VALUES (?,?,?,?,?,SYSDATE())";
+
+    $organisation_name = $request-> getParsedBody()['organisation_name'];
+    $organisation_address = $request-> getParsedBody()['organisation_address'];
+    $organisation_type = $request-> getParsedBody()['organisation_type'];
+    $organisation_phone_number = $request->getParsedBody()['organisation_phone_number'];
+    $organisation_country = $request->getParsedBody()['organisation_country'];
+
+    $stmt = $connection->prepare($query);
+    $stmt->bind_param("sssss",
+        $organisation_name,
+        $organisation_address,
+        $organisation_type,
+        $organisation_phone_number,
+        $organisation_country);
+
+    $stmt->execute();
+
+    if($stmt) {
+
+        $response = "The organisation: " . $organisation_name . " has been added to the database";
+        return $response;
+    }else{
+        echo'error';
+    }
+
+});
--- slim/src/routes/user.php
+++ slim/src/routes/user.php
@@ -40,19 +40,21 @@ $app->get('/view/user',function(){
 });
 
 //insert a user
-$app->post('/add/user',function($request,$response){
+$app->post('/add/user',function($request,$response) use ($password) {
 
     require_once('dbconnect.php');
     $connection = connect_db();
 
     //prepared statements
-    $query = "INSERT INTO `kingsub3_tony`.`tonyoo` (`username`, `name`, `password`) VALUES (?,?,?)";
+    $query = "INSERT INTO kingsub3_FYP.users (user_first_name, user_last_name, user_date_of_birth) VALUES (?,?,?)";
 
     $stmt = $connection->prepare($query);
     $stmt->bind_param("sss", $username, $name, $password);
+
+    $password = $request->getParsedBody()['password'];
     $username = $request->getParsedBody()['username'];
     $name = $request->getParsedBody()['name'];
-    $password = $request->getParsedBody()['password'];
+
     $stmt->execute();
 
     echo($username);
