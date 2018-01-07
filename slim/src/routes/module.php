182a987fb3434cd419e610c26662a0cc8dd0469b
montananyto-dev <k1451757@kingston.ac.uk>
2018-01-07 19:39:59 +0000

    updated module.php

--- slim/src/routes/module.php
+++ slim/src/routes/module.php
@@ -21,35 +21,7 @@ $app->get('/view/module', function () {
         return json_encode($data);
     }
 });
-//
-//$app->get('/return/specific/{id}', function ($request) {
-//
-//    $id = $request->getAttribute('id');
-//    require_once('dbconnect.php');
-//
-//    $connection = connect_db();
-//
-//    $query = "SELECT module.* FROM module
-//              INNER JOIN module_course
-//              ON module.module_id = module_course.module_id
-//              INNER JOIN course
-//              ON module_course.course_id = course.course_id
-//              WHERE course.course_id = $id";
-//
-//    $result = $connection->query($query);
-//
-//    while ($row = $result->fetch_assoc()) {
-//
-//        $data[] = $row;
-//    }
-//    if (isset($data)) {
-//        header('Content-Type: application/json');
-//        return json_encode($data);
-//    }
-//});
 
-
-//testing with an array
 
 $app->get('/return/specific[/{id:.*}]', function ($request, $response, $args) {
 
