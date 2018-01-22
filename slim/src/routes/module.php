3fea29821eadbfa32751a0fb9c556df6a00b02ae
montananyto-dev <montananyto@hotmail.com>
2018-01-22 13:33:44 +0000

    add modules is now working

--- slim/src/routes/module.php
+++ slim/src/routes/module.php
@@ -26,44 +26,38 @@ $app->get('/return/specific[/{id:.*}]', function ($request, $response, $args) {
 
     $id = explode('/', $request->getAttribute('id'));
 
-    $ids = implode(',',$id);
+    $ids = implode(',', $id);
 
+    require_once('dbconnect.php');
 
-        require_once('dbconnect.php');
+    $connection = connect_db();
 
-        $connection = connect_db();
-
-        $query = "SELECT module.* FROM module
+    $query = "SELECT module.* FROM module
               INNER JOIN module_course
               ON module.module_id = module_course.module_id
               INNER JOIN course
               ON module_course.course_id = course.course_id
               WHERE course.course_id IN ($ids)";
 
-        $result = $connection->query($query);
+    $result = $connection->query($query);
 
-        while ($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
 
-            $data[] = $row;
-        }
-        if (isset($data)) {
-            header('Content-Type: application/json');
-            return json_encode($data);
-        }else{
-
-            header('Content-Type: application/json');
-            $response = 'This course do not have any module';
-            return $obj = json_encode($response);
-
+        $data[] = $row;
     }
+    if (isset($data)) {
+        header('Content-Type: application/json');
+        return json_encode($data);
+    } else {
 
-
+        header('Content-Type: application/json');
+        $response = 'This course do not have any module';
+        return $obj = json_encode($response);
+    }
 });
 
 
-
-
-$app->post('/add/module',function($request,$response) {
+$app->post('/add/module', function ($request, $response) {
 
     require_once('dbconnect.php');
     $connection = connect_db();
@@ -71,30 +65,66 @@ $app->post('/add/module',function($request,$response) {
     $validation = json_encode('The module has been added to the system');
     $error = json_encode('The module could not be added to the system');
 
+    $array = $request->getParsedBody();
 
-    //prepared statements
-    $query = "INSERT INTO kingsub3_FYP.module (
+
+    $course_id = $array['course'];
+    $module = $array['module'];
+
+    foreach ($module as $key => $value) {
+
+        $module_year = $module[$key]['moduleYear'];
+        $module_description = $module[$key]['moduleDescription'];
+        $module_name = $module[$key]['moduleName'];
+
+        var_dump($module_year);
+        var_dump($module_description);
+        var_dump($module_name);
+
+        var_dump($key);
+
+        //prepared statements
+        $query = "INSERT INTO kingsub3_FYP.module (
 
     module_name,
     module_year,
     module_description,
     module_created_at)
-    
+
       VALUES (?,?,?,SYSDATE())";
 
-    $module_name = $request->getParsedBody()['moduleName'];
-    $module_year = $request->getParsedBody()['moduleYear'];
-    $module_description = $request->getParsedBody()['moduleDescription'];
+        $stmt = $connection->prepare($query);
 
-    $stmt = $connection->prepare($query);
+        $stmt->bind_param("sss",
 
-    $stmt->bind_param("sss",
+            $module_name,
+            $module_year,
+            $module_description);
 
-        $module_name,
-        $module_year,
-        $module_description);
+        $stmt->execute();
 
-    $stmt->execute();
+        $module_id = mysqli_insert_id($connection);
+
+
+        //prepared statements
+        $query = "INSERT INTO kingsub3_FYP.module_course(
+
+    module_id,
+    course_id)
+    
+      VALUES (?,?)";
+
+        $stmt = $connection->prepare($query);
+
+        $stmt->bind_param("ss",
+
+            $module_id,
+            $course_id
+        );
+
+        $stmt->execute();
+
+    }
 
 
     if ($stmt) {
@@ -105,7 +135,7 @@ $app->post('/add/module',function($request,$response) {
 
         return $error;
 
-
     };
+
 
 });
