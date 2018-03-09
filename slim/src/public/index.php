5993ed1c04bcb1620dda69ad64572dbe876653ac
montananyto-dev <k1451757@kingston.ac.uk>
2018-03-09 01:36:55 +0000

    updated index and dbconnected

--- slim/src/public/index.php
+++ slim/src/public/index.php
@@ -13,7 +13,6 @@ $app = new \Slim\App($settings);
 
 
 require_once('../routes/login.php');
-require_once('../routes/myname.php');
 require_once ('../routes/dbconnect.php');
 require_once ('../routes/user.php');
 require_once('../routes/module.php');
--- slim/src/routes/dbconnect.php
+++ slim/src/routes/dbconnect.php
@@ -1,38 +1,5 @@
 <?php
 
-use Psr\Http\Message\RequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-
-//live connection
-//function connect_db() {
-//    $server = '108.179.213.60'; // this may be an ip address instead
-//    $user = 'kingsub3_tony';
-//    $pass = 'Kingston2017!';
-//    $database = 'kingsub3_FYP';
-//    $port = 3306;
-//    $connection = new mysqli($server, $user, $pass, $database,$port);
-//
-//    return $connection;
-//}
-
-//remember to add your ip on bluehost under
-//Remote Mysql Database Access
-//Add an Access Host
-
-
-//connect locally
-//function connect_db (){
-//    $server = 'localhost'; // this may be an ip address instead
-//    $user = 'root';
-//    $pass = '';
-//    $database = 'kingsub3_fyp';
-//    $connection = new mysqli($server, $user, $pass, $database);
-//
-//    return $connection;
-//}
-
-//connect live DB
-
 try {
     $connection = new PDO('mysql:108.179.213.60;port=3306;dbname=kingsub3_FYP', 'kingsub3_tony', 'Kingston2017!');
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -41,13 +8,3 @@ try {
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
-//
-//function connect_db() {
-//    $server = '108.179.213.60'; // this may be an ip address instead
-//    $user = 'kingsub3_tony';
-//    $pass = 'Kingston2017!';
-//    $database = 'kingsub3_FYP';
-//    $connection = new mysqli($server, $user, $pass, $database);
-//
-//    return $connection;
-//}
